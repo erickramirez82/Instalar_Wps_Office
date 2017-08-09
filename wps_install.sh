6a635d370a2fc11bae526210c5e1bb5bed9bbb45
erickramirez82 <ing.erickfernando@gmail.com>
2017-08-09 16:31:04 -0500

    Archivo para instalar Wps

---
+++ wps_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -eux
+
+## wps office Ubuntu o Debian
+
+#Descargar wps e instalar https://www.wps.com/download
+(
+cd /tmp
+wget "http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb"
+sudo dpkg -i wps-office_10.1.0.5707~a21_amd64.deb
+rm -f wps-office_10.1.0.5707~a21_amd64.deb
+)
+
+#Descargar e instalar Fuentes
+(
+cd /tmp
+wget "https://www.dropbox.com/s/lfy4hvq95ilwyw5/wps_symbol_fonts.zip"
+unzip wps_symbol_fonts.zip
+sudo mkdir /usr/share/fonts/wps-fonts
+sudo cp wps_symbol_fonts/* /usr/share/fonts/wps-fonts
+rm -f wps_symbol_fonts.zip
+rm -rf wps_symbol_fonts
+)
+
+#Descargar e instalar Diccionario y ortografia Español
+(
+cd /tmp
+wget http://wps-community.org/download/dicts/es_ES.zip
+unzip es_ES.zip
+sudo mv es_ES /opt/kingsoft/wps-office/office6/dicts/
+sudo rm es_ES.zip
+)
